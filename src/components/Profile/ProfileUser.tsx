d1fa494075021191d7829db0ab8cb993e0dd3c45
Andreyjkov <Andreyjskov@gmail.com>
2023-02-22 16:03:53 +0300

    feat: disabled btn for profile

--- src/components/Profile/ProfileUser.tsx
+++ src/components/Profile/ProfileUser.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, SyntheticEvent, useEffect, useState } from "react";
+import { Button, Form, InputGroup } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { setUserFirebase } from "../../services";
 import { useAppDispatch, useAppSelector } from "../../store";
 import { setUser } from "../../store/sliceUser";
+import { MdEdit } from "react-icons/md";
 
 interface IProfileForm {
   name: string;
@@ -12,84 +14,85 @@ interface IProfileForm {
 function ProfileUser() {
   const dispatch = useAppDispatch();
   const { email, name, phone } = useAppSelector((store) => store.user);
-  // const [disabled, setDisabled] = useState(true);
+  const [disabled, setDisabled] = useState(true);
 
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<IProfileForm>();
-  // TO DO заставить обновиться инпут последними данными( актульные даные получаю только после перезагрузки)
   const onSubmit = (data: IProfileForm) => {
     dispatch(setUser({ email, ...data }));
     setUserFirebase({ email: email, name: data.name, phone: data.phone });
-    // setDisabled(true);
+    setDisabled(true);
   };
 
-  // console.log(name, phone);
-
-  // useEffect(() => {
-  //   const subscription = watch((value) => {
-  //     if (value.name !== name || value.phone !== phone) {
-  //       setDisabled(false);
-  //     } else {
-  //       setDisabled(true);
-  //     }
-  //   });
-
-  //   return () => subscription.unsubscribe();
-  // }, [disabled, name, phone, watch]);
+  const handleBtnDisabled = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value !== phone && e.target.name === "phone") {
+      setDisabled(false);
+    } else if (e.target.value !== name && e.target.name === "name") {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  };
 
   return (
-    <div className="container">
-      <h3>Имя: {name}</h3>
-      <p>Почта: {email}</p>
-      <p>Телефон: {phone}</p>
-      <div className="card mt-5">
-        <h2 className="text-center">Редактировать</h2>
-        <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
-          <div className="row mb-3">
-            <div className="col-sm-12">
-              <h6 className="mb-0">Имя</h6>
-            </div>
-            <div className="col text-secondary">
-              <input
-                defaultValue={name}
-                {...register("name", { required: true })}
-                type="text"
-                className="form-control"
-              />
-              {errors.name && (
-                <span className="text-danger">Поле должно быть заполнено</span>
-              )}
-            </div>
+    <div className="card mt-5">
+      <h2 className="text-center">Профиль</h2>
+      <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
+        <div className="row mb-3">
+          <div className="col-sm-12">
+            <h6 className="mb-1">Имя</h6>
           </div>
-          <div className="row mb-3">
-            <div className="col-sm-12">
-              <h6 className="mb-0">Телефон</h6>
-            </div>
-            <div className="col text-secondary">
-              <input
-                defaultValue={phone}
-                {...register("phone", { required: true })}
-                type="text"
-                className="form-control"
-              />
-              {errors.phone && (
-                <span className="text-danger">Поле должно быть заполнено</span>
-              )}
-            </div>
-          </div>
-          <div className="text-center">
+          <div className="col text-secondary">
             <input
-              type="submit"
-              className="btn btn-primary px-4"
-              value="Сохранить изменения"
-              // disabled={disabled}
+              {...register("name", {
+                value: name,
+                required: true,
+                onChange: (e) => handleBtnDisabled(e),
+              })}
+              type="text"
+              className="form-control"
             />
+            {errors.name && (
+              <span className="text-danger">Поле должно быть заполнено</span>
+            )}
           </div>
-        </form>
-      </div>
+        </div>
+        <div className="row mb-3">
+          <div className="col-sm-12">
+            <h6 className="mb-1">Телефон</h6>
+          </div>
+          <div className="col text-secondary">
+            <input
+              {...register("phone", {
+                value: phone,
+                required: true,
+                onChange: (e) => handleBtnDisabled(e),
+              })}
+              type="text"
+              className="form-control"
+            />
+            {errors.phone && (
+              <span className="text-danger">Поле должно быть заполнено</span>
+            )}
+          </div>
+        </div>
+        <div className="row mb-3">
+          <h6 className="mb-1">Почта</h6>
+          <div className="col text-secondary">
+            <span className="form-control">
+              {email} <span className="fst-italic"> - нельзя редактировать</span>
+            </span>
+          </div>
+        </div>
+        <div className="text-center">
+          <button className="btn btn-primary" disabled={disabled}>
+            Сохранить изменения
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
